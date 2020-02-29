115ad4d729e8756f123021901a908dcb3eb8c8e0
bhushan2054 <bhl0022@my.londenmet.ac.uk>
2020-02-29 09:08:12 +0545

    distance of detected object in sound with buzzer

--- SmartBlindManStick-v2.py
+++ SmartBlindManStick-v2.py
@@ -1,10 +1,12 @@
+#importing required Libraries
 import RPi.GPIO as GPIO
 import time
 import datetime
 from gtts import gTTS
 import pyttsx3
 import os
- 
+
+#connection of hardwares with GPIO pin of Raspberry Pi
 GPIO.setmode(GPIO.BOARD)
 
 GPIO_TRIGGER = 18
@@ -13,8 +15,8 @@ buzzer = 16
 GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW) 
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-time1= datetime.time
+
+# code for obstactle ditection 
 def distance():
     GPIO.output(GPIO_TRIGGER, True)
  
@@ -34,32 +36,26 @@ def distance():
     distance = (TimeElapsed * 34300) / 2
     return distance
    
- 
 try:
     while True:
         dist = distance()
-        dis = str (dist)
+        distt = round(dist,2)
+        dis = str (distt)
         print ("Measured Distance = %.1f cm" % dist)
         if (dist<=15.0):
             GPIO.output(buzzer,GPIO.HIGH)
             time.sleep(1)
             GPIO.output(buzzer,GPIO.LOW)
             print("Soo near")
+            time.sleep(2)
+        elif(dist>15 and dist<=100):
+            print("far")
             tts_en = gTTS('the object is '+ dis + 'cm ahead ! be alert!!', lang='hi')
             # tts_en.write_to_fp(mp3_fp)
             tts_en.save("distance.mp3")
             os.system("mpg321 -q distance.mp3")
             time.sleep(2)
-            time.sleep(2)
             
-
-
-        else:
-            print("far")
-            time.sleep(2)
-
-
-
             
             
 except KeyboardInterrupt:
